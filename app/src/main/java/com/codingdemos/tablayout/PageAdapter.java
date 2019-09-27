ef1a12b0a2867db23c884cfbfdfb9dc39b18e40e
27 Kim <doohyeonkim@gmail.com>
2019-09-27 21:56:32 +0900

    수정

--- app/src/main/java/com/codingdemos/tablayout/PageAdapter.java
+++ app/src/main/java/com/codingdemos/tablayout/PageAdapter.java
@@ -1,8 +1,10 @@
 package com.codingdemos.tablayout;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 /**
  * Created by abdalla on 2/18/18.
@@ -11,10 +13,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 public class PageAdapter extends FragmentPagerAdapter {
 
     private int numOfTabs;
+    private Context mContext;
 
-    PageAdapter(FragmentManager fm, int numOfTabs) {
+    PageAdapter(FragmentManager fm, int numOfTabs, Context context) {
         super(fm);
         this.numOfTabs = numOfTabs;
+        this.mContext = context;
     }
 
     @Override
@@ -26,6 +30,23 @@ public class PageAdapter extends FragmentPagerAdapter {
                 return new StatusFragment();
             case 2:
                 return new CallFragment();
+            default:
+                return null;
+        }
+    }
+
+
+    // This determines the title for each tab
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.category_usefulinfo);
+            case 1:
+                return mContext.getString(R.string.category_places);
+            case 2:
+                return mContext.getString(R.string.category_food);
             default:
                 return null;
         }
